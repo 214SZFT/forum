89537bb9abf428e062424959b1f3b772e3ec085f
N-J-SZ <nagyhaziszabolcs@turr.hu>
2022-03-01 12:33:42 +0100

    kiindulási állapot

---
+++ Public/js/app.js
@@ -0,0 +1,15 @@
+var app = new angular.module('ForumApp', ['ngRoute', 'ngAnimate']);
+
+app.run(function($rootScope, dbFactory) {
+    $rootScope.title = 'Fórum App';
+    $rootScope.company = 'Bajai SZC Türr István Technikum';
+    $rootScope.author = '2/14.SZFT';
+});
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'View/main.html',
+            controller: 'mainCtrl',
+        })
+});
