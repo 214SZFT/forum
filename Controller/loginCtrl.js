2ab684f658d8808740e14e663cf0915bda0614ad
tamas1023 <bodnar.tamas.1023@gmail.com>
2022-03-08 14:14:46 +0100

    login félig

--- Controller/loginCtrl.js
+++ Controller/loginCtrl.js
@@ -1,17 +1,16 @@
-app.controller('loginCtrl',function ($rootScope,$scope,dbFactory) {
+app.controller('loginCtrl',function ($scope,dbFactory) {
     $scope.loggedIn=0;
-
+    $scope.user={};
 
     $scope.login=function () {
-        console.log("nev: "+$scope.nev);
-        console.log("jelszo: "+$scope.jelszo);
-        if ($scope.nev==null || $scope.jelszo==null) {
-            console.log("nev: "+$scope.nev);
-            console.log("jelszo: "+$scope.jelszo);
+        console.log("nev: "+$scope.user.name);
+        console.log("jelszo: "+$scope.user.password);
+        if ($scope.user.name==null || $scope.user.password==null) {
+            
             alert("Hibás név vagy jelszó!");
 
         } else {
-            
+            dbFactory.select('felhasznalok','nev="${user.name}" AND jelszo='+CryptojS.SHA1("${user.password})" ')
         }
     }
 })
